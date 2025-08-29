e12691e6e4ecc226c5e8ed9738afb50292be384e
michael ciccarelli <mikecicc@gmail.com>
2025-08-29 11:05:08 -0400

    trigger rebuild

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
-          <li>Hello world</li>
+          <li>Hello world.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
